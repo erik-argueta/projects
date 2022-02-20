23f8e833f573959b1c6c8185799bdaaee296724a
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2022-02-19 19:11:16 -0800

    worked on project; added buttons and cleaned code by implementing Theme

--- App-Idea/lib/main.dart
+++ App-Idea/lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
+
+/*
+
+  
+  - Begin Navigation Set-up
+      * Language-Page [Only show up at first initialization of the app]
+      * Main Menu
+      * Vacc Sites
+      * Test Sites
+      * Food Drive
+      * Town Halls
+      * Settings -> Change language?
+      
+  - Find way to cycle the [Please Select a Language]
+*/
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,14 +29,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Language Selection',
-        home: Scaffold(
-            backgroundColor: const Color.fromARGB(255, 54, 57, 63),
-            appBar: AppBar(
-              //backgroundColor: const Color.fromARGB(255, 204, 0, 0),
-              backgroundColor: Colors.green,
-              centerTitle: true,
-              title: const Text('Select a Language'),
-            )));
+      title: 'Language Selection',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 54, 57, 63),
+        colorScheme:
+            ColorScheme.fromSwatch(primarySwatch: Colors.green).copyWith(
+          secondary: Colors.green,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Please Select a Language'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              // ENGLISH
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.only(
+                    left: (46.5),
+                    right: (46.5),
+                  ),
+                  textStyle: const TextStyle(fontSize: 30),
+                ),
+                onPressed: () {},
+                child: const Text('English'),
+              ),
+              // SPANISH
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.only(
+                    left: (42),
+                    right: (42),
+                  ),
+                  textStyle: const TextStyle(fontSize: 30),
+                ),
+                onPressed: () {},
+                child: const Text('Espanol'),
+              ),
+              // MANDARIN
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.only(
+                    left: 32,
+                    right: 32,
+                  ),
+                  textStyle: const TextStyle(fontSize: 30),
+                ),
+                onPressed: () {},
+                child: const Text('Mandarin'),
+              ),
+              // VIETNAMESE
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 30),
+                ),
+                onPressed: () {},
+                child: const Text('Vietnamese'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
